1ffbdacd19178c3619e38c9a752abba594386758
frc <fcaumartin@gmail.com>
2016-03-22 15:15:46 +0100

    premier script PHP

---
+++ PHP/script1.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<?php
+	$nom = "toto";
+	$age = 0;
+
+	$test = $nom + $age;
+
+	echo $test;
+?>
+
+<?php
+
+	// for($i=0; $i<10; $i++) {
+	// 	echo "<b>coucou</b><br>";
+	// }
+?>
+
+
+</body>
+</html>
